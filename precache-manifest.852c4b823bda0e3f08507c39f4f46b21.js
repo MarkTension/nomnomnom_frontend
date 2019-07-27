5572d63843c567fd89232d490d91a6fd6729a7ab
MarkTension <tensen.mark@gmail.com>
2019-07-27 14:05:52 +0200

    Updates

---
+++ precache-manifest.852c4b823bda0e3f08507c39f4f46b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3fdf9172d487f0d4e637b5111bd22f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1bc6c8ba37a1198333d",
+    "url": "/static/css/main.19637d4c.chunk.css"
+  },
+  {
+    "revision": "e51667e49592734c7f7a",
+    "url": "/static/js/2.2a44cda1.chunk.js"
+  },
+  {
+    "revision": "e1bc6c8ba37a1198333d",
+    "url": "/static/js/main.4cc8f82b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0aed1bb0bf4ddb7e93a190236447ff4e",
+    "url": "/static/media/logo.0aed1bb0.jpeg"
+  },
+  {
+    "revision": "3c3a5221589e99838f748ad37db06e02",
+    "url": "/static/media/nomnom_logo.3c3a5221.png"
+  }
+]);
