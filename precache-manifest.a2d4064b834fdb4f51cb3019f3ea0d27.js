7314b3ab2fc1821dbf7bb55a9aadbce7401cc227
MarkTension <tensen.mark@gmail.com>
2019-07-27 18:19:40 +0200

    Updates

---
+++ precache-manifest.a2d4064b834fdb4f51cb3019f3ea0d27.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7518f3325a678d2765ec527ad3e44b18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e20fe85f3ea970fcb73f",
+    "url": "/static/css/main.dcff65c3.chunk.css"
+  },
+  {
+    "revision": "0b7f8f2fb846126c8b57",
+    "url": "/static/js/2.98c5911a.chunk.js"
+  },
+  {
+    "revision": "e20fe85f3ea970fcb73f",
+    "url": "/static/js/main.d2a7ad1e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e36e4e65309004be07fb8904098543c9",
+    "url": "/static/media/korean.e36e4e65.jpeg"
+  },
+  {
+    "revision": "4bbda675b91d91410cbba93be55db29f",
+    "url": "/static/media/mexican.4bbda675.jpeg"
+  },
+  {
+    "revision": "3c3a5221589e99838f748ad37db06e02",
+    "url": "/static/media/nomnom_logo.3c3a5221.png"
+  },
+  {
+    "revision": "8e5bb59ee17e7e2478f98189f5f0df63",
+    "url": "/static/media/pizza.8e5bb59e.jpeg"
+  },
+  {
+    "revision": "3e5740356c1e6a9229524164fef22e4f",
+    "url": "/static/media/sea.3e574035.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.852c4b823bda0e3f08507c39f4f46b21.js"
+  "/precache-manifest.a2d4064b834fdb4f51cb3019f3ea0d27.js"
 );
 
 self.addEventListener('message', (event) => {
